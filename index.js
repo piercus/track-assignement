ed34977f83f66e8b4022fd5faab47829f3848226
Pierre Colle <piercus@gmail.com>
2023-04-20 17:18:59 +0200

    fix: exposed keys

--- index.js
+++ index.js
@@ -1,12 +1,4 @@
 module.exports = {
 	GlobalMatching: require('./lib/global-matching'),
-	Tracker: require('./lib/tracker'),
-	ProjectiveTracker: require('./lib/projective-tracker'),
-	Projection: require('./lib/projection'),
-	projectObservation: require('./lib/utils/project-observation'),
-	dynamic: require('./lib/dynamic'),
-	detection2track: require('./lib/distances/detection2track'),
-	observation: require('./lib/observation'),
-	strategies: require('./lib/strategies'),
-	getFusioner: require('./lib/utils/fusion/get-fusioner')
+	MatchingCascade: require('./lib/matching-cascade')
 };
