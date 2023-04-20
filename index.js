5e6cef38233f22538ecb40fd5de877b0cf000264
Pierre Colle <piercus@gmail.com>
2023-04-21 01:17:43 +0200

    fix: distances

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 module.exports = {
 	GlobalMatching: require('./lib/global-matching'),
 	MatchingStage: require('./lib/matching-stage'),
+	Tracker: require('./lib/tracker'),
 	getDistances: require('./lib/matching-cascade/get-distances'),
 	lambdaSumDistances: require('./lib/matching-cascade/lambda-sum-distances')
 };
